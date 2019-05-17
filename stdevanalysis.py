d639d3848f851b492291e886794df93e441ade7a
AanchalSingla <44216192+AanchalSingla@users.noreply.github.com>
2019-05-16 19:45:50 -0700

    Update stdevanalysis.py

--- stdevanalysis.py
+++ stdevanalysis.py
@@ -3,24 +3,30 @@ from matplotlib import pyplot as plt
 
 n = [30/400, 30/500, 30/600, 30/700, 30/800]
 
-stdev = [1.6527150377497226, 1.6523010313744455, 1.6520143553026985, 1.6518049933779948, 1.6516465489646033]
 
-comptime = [0.04291653633117676, 0.04487895965576172, 0.04787087440490723, 0.04886889457702637, 0.05385589599609375]
+stdev = [0.010900626719684565, 0.0036860616547505953, 0.002239910796947778, 0.002030006740653487, 0.002005339746723398]
+
+comptime = [0.19494128227233887, 0.2164759635925293, 0.2624847888946533, 0.33491063117980957, 0.33598995208740234]
 
 fig, ax1 = plt.subplots()
 
 color = 'tab:red'
 ax1.set_xlabel('time step')
-ax1.set_ylabel('stdev', color=color)
-ax1.plot(n, stdev, color=color)
+
+
+ax1.set_ylabel('comp_time', color=color)
+ax1.plot(n, comptime, color=color)
 ax1.tick_params(axis='y', labelcolor=color)
 
-ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+ax2 = ax1.twinx()
 
 color = 'tab:blue'
-ax2.set_ylabel('comp_time', color=color)  # we already handled the x-label with ax1
-ax2.plot(n, comptime, color=color)
+ax2.set_ylabel('stdev', color=color)
+ax2.plot(n, stdev, color=color)
 ax2.tick_params(axis='y', labelcolor=color)
 
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig.tight_layout()  
 plt.show()
+
+
+
